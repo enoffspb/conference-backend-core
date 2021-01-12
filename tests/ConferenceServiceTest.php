b312054f867d9d5f2342b0c801bb64f4a335b071
Ilya Pastukhov <enoff.spb@gmail.com>
2021-01-12 23:28:44 +0300

    Add todos to tests

--- tests/ConferenceServiceTest.php
+++ tests/ConferenceServiceTest.php
@@ -19,4 +19,32 @@ class ConferenceServiceTest extends  TestCase
     {
         $this->assertNotNull($this->conferenceService);
     }
+
+    public function testCreateConference()
+    {
+        // @todo Test method ConferenceService->createConference()
+
+        throw new \Exception('Method is not implemented.');
+    }
+
+    public function testAddUser()
+    {
+        // @todo Test method ConferenceService->addUser()
+
+        throw new \Exception('Method is not implemented.');
+    }
+
+    public function testKickUser()
+    {
+        // @todo Test method ConferenceService->kickUser()
+
+        throw new \Exception('Method is not implemented.');
+    }
+
+    public function testCloseConference()
+    {
+        // @todo Test method ConferenceService->closeConference()
+
+        throw new \Exception('Method is not implemented.');
+    }
 }
